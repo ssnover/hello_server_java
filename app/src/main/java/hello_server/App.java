81ffcb9d7d20d8b41534801996c7a1d215af90d1
Shane Snover <ssnover95@gmail.com>
2024-03-30 18:22:46 -0600

    Start reading in request data from incoming connection

--- app/src/main/java/hello_server/App.java
+++ app/src/main/java/hello_server/App.java
@@ -1,9 +1,12 @@
 package hello_server;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws IOException {
@@ -13,7 +16,19 @@ public class App {
 
         while (true) {
             Socket stream = listener.accept();
-            System.out.println("Conenction established!");
+            handle_connection(stream);
         }
+    }
+
+    public static void handle_connection(Socket stream) throws IOException {
+        var buf_reader = new BufferedReader(new InputStreamReader(stream.getInputStream()));
+
+        ArrayList<String> request_lines = new ArrayList<>();
+        String line;
+        while ((line = buf_reader.readLine()) != null && !line.isEmpty()) {
+            request_lines.add(line);
+        }
+        System.out.println("Request: " + request_lines.toString());
+        stream.close();
     }
 }
