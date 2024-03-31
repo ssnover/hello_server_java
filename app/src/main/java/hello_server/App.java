d0667be93cd907259b5459234b74e22b4e8068ca
Shane Snover <ssnover95@gmail.com>
2024-03-30 18:50:08 -0600

    Completed single threaded web server

--- app/src/main/java/hello_server/App.java
+++ app/src/main/java/hello_server/App.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class App {
@@ -22,13 +24,25 @@ public class App {
 
     public static void handle_connection(Socket stream) throws IOException {
         var buf_reader = new BufferedReader(new InputStreamReader(stream.getInputStream()));
+        String request_line = buf_reader.readLine();
 
-        ArrayList<String> request_lines = new ArrayList<>();
-        String line;
-        while ((line = buf_reader.readLine()) != null && !line.isEmpty()) {
-            request_lines.add(line);
+        String status_line = null;
+        String filename = null;
+
+        if (request_line.equals("GET / HTTP/1.1")) {
+            status_line = "HTTP/1.1 200 OK";
+            filename = "hello.html";
+        } else {
+            status_line = "HTTP/1.1 404 NOT FOUND";
+            filename = "404.html";
         }
-        System.out.println("Request: " + request_lines.toString());
+
+        String contents = Files.readString(Paths.get(filename));
+        var length = contents.length();
+
+        String response = String.format("%s\r\nContent-Length: %d\r\n\r\n%s", status_line, length, contents);
+        stream.getOutputStream().write(response.getBytes());
+
         stream.close();
     }
 }
